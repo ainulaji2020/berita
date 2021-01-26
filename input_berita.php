3bba54d1dec5b24e30b92df9fcfba2b787c9e2a0
ainulaji2020 <ainulaji163@gmail.com>
2021-01-26 19:23:57 +0700

    Upload berita layout

---
+++ input_berita.php
@@ -0,0 +1,286 @@
+<?php  
+$arr = [
+    [
+        "image" => "mic.jpg",
+        "text" => "Tujuan dan Manfaat Konferensi Pers / Press Conference",
+        "tanggal" => "31-jul-2020"
+    ],
+    [
+        "image" => "keluarga.jpg",
+        "text" => "Pentingnya Media Relations bagi Perusahaan & Lembaga",
+        "tanggal" => "31-jul-2020"
+    ],
+    [
+        "image" => "kamera.jpg",
+        "text" => "Pengertian Press Briefing / Jumpa Pers dan Hal-hal yang perlu diperhatikan",
+        "tanggal" => "31-jul-2020"
+    ],
+
+    //image ke 2 array//
+
+    [
+        "image" => "kamera.jpg",
+        "text" => "Pengertian Press Briefing / Jumpa Pers dan Hal-hal yang perlu diperhatikan",
+        "tanggal" => "31-jul-2020"
+    ],
+
+    [
+        "image" => "kamera.jpg",
+        "text" => "Pengertian Press Briefing / Jumpa Pers dan Hal-hal yang perlu diperhatikan",
+        "tanggal" => "31-jul-2020"
+    ],
+
+    [
+        "image" => "kamera.jpg",
+        "text" => "Pengertian Press Briefing / Jumpa Pers dan Hal-hal yang perlu diperhatikan",
+        "tanggal" => "31-jul-2020"
+    ]
+
+];
+
+// echo $arr[1][""];
+// echo "<pre>";
+// 	print_r($arr);
+
+// echo "</pre>";
+// die();
+?>
+<html>
+	<head>
+		<title>Membuat berita</title>
+		<link rel="stylesheet" type="text/css" href="asset/output_berita.css" />
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<div class="banner-top">
+			<div class="inbannertop">
+				<?php  
+				echo date("l ,d-M-Y");
+				?>
+				<a href="#">Publik Media Online</a>
+			</div>
+			<div style= "background-color: #fff;" class="inbannertop">
+				<div class="conten-image-top">
+					<img src="asset/image/digital.jpg">
+				</div>
+			</div>
+		</div>
+		<div class="pembuungkus-konten">
+			<div class="conten-nav">
+				<nav>
+					<ul>
+						<li><a href="home">HOME</a></li>
+						<li><a href="marketing">MARKETING</a></li>
+						<li><a href="publik realition">PUBLIC REALITION</a></li>
+						<li><a href="development">DEVELOPMENT</a></li>
+						<li><a href="design">DESIGN</a></li>
+						<li><a href="business">BUSINESS</a></li>
+						<li><a href="inspiration">INSPIRATION</a></li>
+						<li><a href="contact us">CONTACT US</a></li>
+					</ul>
+					<div class="clearfix"></div>
+				</nav>
+			</div>
+			<div class="wrap-coun">
+				<div class="conten-center">
+					<div class="countainer">
+						<div class="text">
+							<a style="font-size: 14px; text-decoration: none;" href="publik relation">PUBLIK RELATION</a>
+							<h1>13 Portal Berita Online Terbaik di Indonesia</h1>
+						</div>
+						<img class="image-1" src="asset/image/portal-berita.jpg" />
+						<p> Dulu untuk menerima sebuah info terutama berita kita perlu membeli sebuah media cetak seperti koran atau majalah. Tetapi seiring perkembangan teknologi kita bisa dengan mudah mengaksesnya via website / situs. Pertama kali situs berita online di Indonesia sejak tahun 90-an, situs portal berita menjadi bagian yang berjalan beriringan dengan majunya teknologi internet.</p>
+						<p>Tiap harinya kita selalu disuguhkan beraneka ragam pemberitaan dan berita bermacam-macam. Ada berita politik, ekonomi, olahraga, sosial bahkan sampai public figure seperti artis. Membaca berita tentu membuat kita tidak ketinggalan informasi terupdate dan bisa memperkaya wawasan.</p>
+						<p>Sekarang tampilan bermacam situs <a style="text-decoration: none;" href="portal-berita">portal berita</a> Indonesia sudah benar-benar menarik dan nyaman untuk diperhatikan. Dengan ditambah artikel menarik, tentu membikin pembaca tertarik dan memberikan info yang membangun. Tiap-tiap orang saat ini dapat mengakses internet dengan gampang dimana saja mereka berada.</p>
+						<h3>Berikut 13 portal berita online terbaik di Indonesia :</h3>
+						<h2>1. Tribunnews.com</h2>
+						<p>Tribunnews merupakan situs berita online di Indonesia yang mempunyai slogan “berita terkini indonesia”. Situs ini menyampaikan berita terupdate lokal, nasional, internasional, ekonomi, olahraga serta seleb. Pada tahun 2016, situs berita ini pernah dinobatkan sebagai website portal terbaik di Indonesia.</p>
+
+						<h2>2. Detik.com</h2>
+						<p>Didirikan pada tahun 1998, Detikcom bisa disebut sebagai pencetus perusahaan media online di Indonesia. Sesuai seperti nama nya, portal ini menawarkan berita baru tiap detik dan tentunya dan tentunya menjadi portal informatif cepat dan terkini di Indonesia.</p>
+
+						<h2>3. Kompas.com</h2>
+						<p>Dari media cetak yang sekarang dikenal dengan kompas tv beralih ke portal web berita dengan alamat website kompas.com. Kompas.com selalu membagikan informasi yang terkait dengan dunia ekonomi, olahraga, teknologi, dan masih banyak lagi.</p>
+
+						<h2>4. Liputan6.com</h2>
+						<p>Berawal dari acara televisi, Liputan6 beralih ke situs berita online. Situs yang menyajikan bermacam artikel terbaik tajam, aktual, dan terpercaya seputar peristiwa yang sedang marak terjadi seperti politik, olahraga, media sosial, kriminalitas, dan lain-lain.</p>
+
+						<h2>5. Merdeka.com</h2>
+						<p>Pada mulanya merdeka.com bukanlah bermula dari pengembangan media melainkan merdeka.com justru dibangun oleh perusahaan teknologi menguasai tentang bahasa pemrograman PHP & Apache/FreeBSD. Bergabung dan bersinergi dengan portal web kapanlagi.com, membikin merdeka menjadi salah satu portal berita terbaik dan demikian itu dikenali masyarakat.</p>
+
+						<h2>6. Kapanlagi.com</h2>
+						<p>KapanLagi.com adalah Portal Berita Online di Indonesia yang berfokus pada berita dunia hiburan. Memperkenalkan bermacam berita yang terkait dengan musik dan dangdut tanah air.</p>
+
+						<h2>7. Okezone.com</h2>
+						<p>Berdiri sejak tahun 2007, Okezone adalah portal berita dan hiburan yang terbaik dan terpercaya yang berpusat pada pembaca Indonesia baik dalam negeri ataupun luarnegeri.</p>
+
+						<h2>8. Tempo.co</h2>
+						<p>Tempo sebelum turun ke berita online adalah salah satu koran dan majalah terpopuler di indonesia. Tempo.co didirikan oleh PT Tempo Inti Media, Tbk.</p>
+
+						<h2>9. Viva.co.id</h2>
+						<p>Viva.co.id yang sebelumnya bernama vivanews.com dan resmi didirikan pada tahun 2008 oleh PT Visi Media Asia Tbk. Portal web berita tersebut menyampaikan warta harian terupdate tentang politik dan lifestyle.</p>
+
+						<div class="berita-conten">
+							<div>Baca Juga :<a href="pasang berita diportal media online indonesia"><i> Pasang Berita di Portal Media Indonesia</i></a></div>
+						</div>
+
+						<h2>10. Suara.com</h2>
+						<p>Portal website Suara.com tidak kalah bagus dengan portal web berita lainnya. Portal web ini yaitu salah satu situs portal berita digital terpopuler di Indonesia. Portal berita yang cukup lengkap dikemas dalam desain yang bagus sehingga pembaca bisa mengaksesnya lebih gampang.</p>
+
+						<h2>11. JPNN.com</h2>
+						<p>JPNN.com merupakan salah satu portal berita besar di Indonesia dengan rank 50 besar di Indonesia menurut Alexa. JPNN sendiri terus menyajikan berita terupdate & terpopuler seputar berita ekonomi, travel, olahraga, criminal, sepak bola dan lain-lain</p>
+
+						<h2>12. Sindonews.com</h2>
+						<p>Sindonews.com berdiri di tahun 2012 dan sekarang juga menempati salah satu posisi teratas di bidangnya.  Menyajikan berbagai macam berita yang bisa di akses secara online.</p>
+
+						<h2>13. Jawapos.com</h2>
+						<p>Jawapos.com dimiliki oleh Jawapos Group juga menempati Posisi terbaik dalam media online, selain sudah lama berdiri mereka berhasil berubah dari konvensional menjadi portal berita online di Indonesia.</p>
+						<p>Diatas kurang lebih media online terbaik di Indonesia dengan selalu update & menyajikan informasi informasi terupdate & terkini. Kalian ingin diberitakan di media online tersebut? <a href="klik disini">Klik Disini</a></p><br/>
+
+						<h1>Anda Punya Pertanyaan ?</h1>
+
+						<from action="output_berita.php" method="POST">
+							Your Name (*)<br/>
+							<input class="from" type="text" name="nama"><br/><br/>
+
+							Your Email (*)<br/>
+							<input class="from" type="text" name="email"><br/><br/>
+
+							Phone (*)<br/>
+							<input class="from" type="text" name="phone"><br/><br/>
+
+							Your Message<br/>
+							<textarea name="Message"></textarea><br/><br/>
+
+							<input class="buttuon-submit" type="submit" value="SEND">
+						</from><br/><br/>
+
+						<h4><ins>RELATED POSTS</ins></h4>
+						<div class="wrap-box">
+							<?php  
+								// foreach ($arr as $key => $value) {
+								for($i=0; $i< count($arr);$i++){
+									if ($i<=2) {
+										
+									
+							?>
+										<div class="box">
+											<a href="#"><img style="width: 100%;" src="asset/image/<?php echo $arr[$i]["image"]; ?>" /></a>
+											<a href="#"><?php echo $arr[$i]["text"]; ?> </a><br/>
+											<a href="#"><?php echo $arr[$i]["tanggal"]; ?></a>	
+										</div>
+							<?php  
+									}
+								}
+								// }
+							?>
+							
+							<div class="clearfix"></div>
+						</div>
+						<div class="wrap-box">
+							<div class="box1">
+								<a href="#" style="width: 100%; display: block; color: grey; padding-bottom: 15px;">PREVISOUS ARTICLE</a>
+								<a  href="#">Jenis-Jenis Iklan Online yang perlu kalian ketahui</a>
+							</div>
+							<div class="box1">
+								<a href="#" style="width: 100%; text-align: right; display: block; color: grey; padding-bottom: 15px;">NEXT ARTICLE</a>
+								<a style="padding-left: 50px;" href="#">Apa itu Media Planning dan Media Buying?</a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="conten-bottom">
+							<div class="box-image">
+								<img class="image-bottom" src="asset/image/orang.png" />
+							</div>
+							<div style="width: 45%; padding-top: 30px;" class="box-image">
+								<p style="padding-left: 20px;">About Shelly Maysari</p>
+								<a style="padding-left: 20px; text-decoration: none;" href="#"> View all posts by Muhamad Ainul Aji <i style='font-size:15px' class='fa fa-'>&#xf061;</i></a>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- //ini konten yang di kanan // -->
+				<div class="countainer-right">
+						<input class="input-right" type="" name="submit" value="Search...."><input style="width: 20%;" class="buttuon-submit" type="submit" value="SEARCH">
+						
+						<h4><ins>PUBLIKASI MEDIA ONLINE</ins></h4>
+						<div class="box-side-right">
+							<a href="#"><img src="asset/image/our-clients.jpg" /></a>
+						</div>
+						<h4><ins>POPULAR ARTICLES</ins></h4>
+						<div class="wrap-box1">
+							<?php  
+								foreach ($arr as $key => $value) {
+							?>
+								<!-- <div class="box">
+									<a href="#"><img style="width: 100%;" src="asset/image/<?php echo $value["image"]; ?>" /></a>
+									<a href="#"><?php echo $value["text"]; ?> </a><br/>
+									<a href="#"><?php echo $value["tanggal"]; ?></a>
+								</div> -->
+							<?php  
+
+								}
+							?>
+						</div>
+						<div class="vidio-link">
+							<h4><ins>PUBLIKASI MEDIA ONLINE</ins></h4>
+							<video controls>
+								<source src="asset/vidio/vidio-dunia.mp4" type="video/mp4">
+							</video>
+						</div>
+
+						<h4><ins>PUBLIKASI MEDIA ONLINE</ins></h4>
+						<div class="box-side-right">
+							<a href="#"><img src="asset/image/our-clients.jpg" /></a>
+						</div>
+
+					</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+
+		<div class="clearfix"></div>
+
+		<div class="footer">
+			<div class="wrap-footer">
+				<div class="box1-footer">
+					<p><u>AKU DIGITAL</u></p>
+					<img style="background-image: #333;" src="asset/image/digital2.jpg" />
+					<p>Digital Marketing, Advertising & Public Relation Indonesia.</p>
+					<p>Our Focus to help you grow through Digital Marketing</p>
+				</div>
+
+				<div class="box1-footer">
+					<p><u>AKU DIGITAL</u></p>
+
+					<div class="minibox">
+						<img style="float: left; width: 40%;" src="asset/image/kamera.jpg" />
+						<div class="minibox-description">
+							<a style="text-decoration: none; color: #fff; display: block; padding-bottom: 10px;" href="#">Biografi Warren Buffet: Profil Orang Terkaya di Dunia</a>
+							<p><?php echo date("l ,d-M-Y"); ?></p>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+
+				<div class="box1-footer">
+					<p><u>AKU DIGITAL</u></p>
+
+					<div class="minibox">
+						<img style="float: left; width: 40%;" src="asset/image/kamera.jpg" />
+						<div class="minibox-description">
+							<a style="text-decoration: none; color: #fff; display: block; padding-bottom: 10px;" href="#">Biografi Warren Buffet: Profil Orang Terkaya di Dunia</a>
+							<p><?php echo date("l ,d-M-Y"); ?></p>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</body>
+</html>
